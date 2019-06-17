65f4e8e0a5434e0fd6de2d4274d9aacece3bc438
minsu <karosis28@gmail.com>
2019-06-17 22:14:44 +0900

    --Chapter7 핵심 기능의 실행은 다른 객체에 위임하고 부가적인 기능을 제공하는 객체 프록시 구현

---
+++ src/main/java/main/MainProxy.java
@@ -0,0 +1,17 @@
+package main;
+
+import spring.ExeTimeCalculator;
+import spring.ImpeCalculator;
+import spring.RecCalculator;
+
+public class MainProxy {
+
+    public static void main(String[] args) {
+
+        ExeTimeCalculator ttCal1 = new ExeTimeCalculator(new ImpeCalculator());
+        System.out.println(ttCal1.factorial(20));
+
+        ExeTimeCalculator ttCal2 = new ExeTimeCalculator(new RecCalculator());
+        System.out.println(ttCal2.factorial(20));
+    }
+}
